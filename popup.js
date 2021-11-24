bd44d73a3b69d06d4ce7fe56be4a682007361608
knshika <poojakumari17032001@gmail.com>
2021-11-24 20:41:31 +0530

    🐛 cleaned up code

--- popup.js
+++ popup.js
@@ -1,46 +1,56 @@
-//https://www.linkedin.com/search/results/people/?keywords=sde%20amazon&origin=SWITCH_SEARCH_VERTICAL&sid=*6-
-
-//conent-script
-const clickConnectBtns = async () => {
-  let buttons = document.querySelectorAll("[aria-label^='Invite']");
-  let count = 0;
+/**
+ * This function gets injected into the linkedin webpage
+ * It needs to contain all the function it needs inside it as the context will be lost
+ * see also: https://developer.chrome.com/docs/extensions/reference/scripting/#runtime-functions
+ */
+const injectedFunc = async () => {
   const wait = async (ms) => {
     return new Promise((resolve) => {
       setTimeout(resolve, ms);
     });
   };
 
-  function getRandomTime(min, max) {
+  const getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
-  }
+  };
 
+  const buttons = document.querySelectorAll("[aria-label^='Invite']");
+  const total = buttons.length;
+
+  let count = 0;
   for (const button of buttons) {
     button.click();
-
-    await wait(getRandomTime(2000, 3000));
+    await wait(getRandom(2000, 3000));
 
     let send = document.querySelector("[aria-label^='Send']");
-    if (send) {
-      send.click();
-      chrome.runtime.sendMessage(++count);
-    }
-    await wait(getRandomTime(2000, 3000));
+    if (send) send.click();
+
+    count++;
+    chrome.runtime.sendMessage({ count, total });
+    await wait(getRandom(2000, 3000));
   }
 };
 
-//background-script
-const injectTheScript = () => {
+const progressbar = document.querySelector(".progress");
+const sendrequest = document.querySelector("#send-request");
+const requeststatus = document.querySelector("#request-status");
+
+chrome.runtime.onMessage.addListener((response) => {
+  const progressPerc = (response.count / response.total) * 100;
+  const isDone = response.total === response.count;
+
+  sendrequest.disabled = !isDone;
+  requeststatus.innerText = response.count;
+  progressbar.style.width = `${progressPerc}%`;
+});
+
+const startSendRequests = () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
     chrome.scripting.executeScript({
       target: { tabId: tab[0].id },
-      func: clickConnectBtns,
-    });
-    chrome.runtime.onMessage.addListener(function (response) {
-      document.querySelector("#request-status").innerText = response;
+      func: injectedFunc,
     });
   });
 };
 
-document
-  .querySelector("#send-request")
-  .addEventListener("click", injectTheScript);
+sendrequest.addEventListener("click", startSendRequests);
